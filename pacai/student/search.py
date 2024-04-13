746b4d85e68f14eaa2a16fb771dd0b547aa708f9
dekramer-business <dekramer.business@gmail.com>
2024-04-13 15:18:46 -0700

    yea this is so bad and inoptimal

--- pacai/student/search.py
+++ pacai/student/search.py
@@ -18,8 +18,53 @@ def depthFirstSearch(problem):
     ```
     """
 
+    print("Start: %s" % (str(problem.startingState())))
+    print("Is the start a goal?: %s" % (problem.isGoal(problem.startingState())))
+    print("Start's successors: %s" % (problem.successorStates(problem.startingState())))
+
     # *** Your Code Here ***
-    raise NotImplementedError()
+    if problem.isGoal(problem.startingState()):
+        return []
+    
+    frontier = [problem.startingState()]  # stack for DFS
+    explored = set()
+    parents = {}
+    solution = []
+
+    while True:
+        if len(frontier) == 0:  # if frontier empty, failure
+            return
+
+        current_node = frontier.pop()
+        
+        if problem.isGoal(current_node):
+            print("Goal state: %s" % (str(current_node)))
+            # print("Explored: %s" % (explored))
+            print("Parents: %s" % (parents))
+
+            list_thang = []
+            curr = current_node
+            while curr != None:
+                list_thang.append(curr)
+                curr = parents.get(curr)
+            
+            print("list_thang: %s" % (list_thang))
+
+            return explored
+        
+        explored.add(current_node)
+
+        # starts left, could be reversed
+        for (neighbor, direction, weight) in problem.successorStates(current_node):
+            # print("%s's Neighbor: %s" % (current_node, neighbor))
+            if (neighbor not in frontier) and (neighbor not in explored):
+                frontier.append(neighbor)
+                parents[neighbor] = current_node
+                # parents[neighbor] = direction
+
+
+
+    # raise NotImplementedError()
 
 def breadthFirstSearch(problem):
     """
