60a7da4d451c18c7e37049e8bbdfc4318c3658f9
dekramer-business <dekramer.business@gmail.com>
2024-04-13 19:46:44 -0700

    UCS implemented, i think. A star hasnt been worked on yet. Need to figure out how to calculate g(), i already have h()

--- pacai/student/search.py
+++ pacai/student/search.py
@@ -1,6 +1,7 @@
 """
 In this file, you will implement generic search algorithms which are called by Pacman agents.
 """
+from queue import PriorityQueue
 
 def depthFirstSearch(problem):
     """
@@ -69,6 +70,10 @@ def breadthFirstSearch(problem):
     """
     Search the shallowest nodes in the search tree first. [p 81]
     """
+    print("Start: %s" % (str(problem.startingState())))
+    print("Is the start a goal?: %s" % (problem.isGoal(problem.startingState())))
+    print("Start's successors: %s" % (problem.successorStates(problem.startingState())))
+    print("succesor's directions type: ", type((problem.successorStates(problem.startingState()))[0][1]))
 
     # *** Your Code Here ***
     if problem.isGoal(problem.startingState()):
@@ -110,7 +115,6 @@ def breadthFirstSearch(problem):
                 # set neighbor's parent and parent->neighbor direction here
                 # can also set path weight and use that to calculate the full path at the end
                 parents[neighbor] = (current_node, direction)
-    raise NotImplementedError()
 
 def uniformCostSearch(problem):
     """
@@ -118,7 +122,48 @@ def uniformCostSearch(problem):
     """
 
     # *** Your Code Here ***
-    raise NotImplementedError()
+    if problem.isGoal(problem.startingState()):
+        return []
+    
+    frontier = PriorityQueue()  # priority queue for UCS
+    frontier.put((0, problem.startingState()))  # starting state has a cost of 0
+
+    explored = set()  # init empty set
+    parents = {}
+
+    while True:
+        if frontier.qsize() == 0:  # if frontier empty, failure
+            return
+
+        # using priority queue for UCS
+        frontier_get = frontier.get()
+        (path_weight, current_node) = frontier_get
+        
+        # if this is the goal state, generate a path from goal to start via parents dictionary
+        if problem.isGoal(current_node):
+            directions = []
+            while True:
+                parent = parents.get(current_node)
+                if parent is None:  # when get returns None, we found the full path
+                    break
+                else:  # otherwise unpack the next node and the direction it took
+                    (current_node, direction) = parent 
+
+                directions.append(direction)
+
+            # directions are in reverse, flip the list. This is faster than prepending every time in native python
+            directions.reverse()
+            return directions
+        
+
+        explored.add(current_node)
+        # unpack neighbor, direction, and path weight for each neighboring node
+        for (neighbor, direction, weight) in problem.successorStates(current_node):
+            if (neighbor not in frontier.queue) and (neighbor not in explored):
+                frontier.put((weight, neighbor))  # add neighbor and path weight to frontier
+                # set neighbor's parent and parent->neighbor direction here
+                # can also set path weight and use that to calculate the full path at the end
+                parents[neighbor] = (current_node, direction)
 
 def aStarSearch(problem, heuristic):
     """
@@ -126,4 +171,45 @@ def aStarSearch(problem, heuristic):
     """
 
     # *** Your Code Here ***
-    raise NotImplementedError()
+    if problem.isGoal(problem.startingState()):
+        return []
+    
+    frontier = PriorityQueue()  # priority queue for UCS
+    frontier.put((0, problem.startingState()))  # starting state has a cost of 0
+
+    explored = set()  # init empty set
+    parents = {}
+
+    while True:
+        if frontier.qsize() == 0:  # if frontier empty, failure
+            return
+
+        # using priority queue for UCS
+        frontier_get = frontier.get()
+        (path_weight, current_node) = frontier_get
+        
+        # if this is the goal state, generate a path from goal to start via parents dictionary
+        if problem.isGoal(current_node):
+            directions = []
+            while True:
+                parent = parents.get(current_node)
+                if parent is None:  # when get returns None, we found the full path
+                    break
+                else:  # otherwise unpack the next node and the direction it took
+                    (current_node, direction) = parent 
+
+                directions.append(direction)
+
+            # directions are in reverse, flip the list. This is faster than prepending every time in native python
+            directions.reverse()
+            return directions
+        
+
+        explored.add(current_node)
+        # unpack neighbor, direction, and path weight for each neighboring node
+        for (neighbor, direction, weight) in problem.successorStates(current_node):
+            if (neighbor not in frontier.queue) and (neighbor not in explored):
+                frontier.put((weight, neighbor))  # add neighbor and path weight to frontier
+                # set neighbor's parent and parent->neighbor direction here
+                # can also set path weight and use that to calculate the full path at the end
+                parents[neighbor] = (current_node, direction)
