9e69f747cdff13d26860d7d6568e7a2393bbf89d
dekramer-business <dekramer.business@gmail.com>
2024-06-02 16:21:10 -0700

    passes script 5/5

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -1,10 +1,6 @@
-from pacai.util import reflection
 from pacai.agents.capture.reflex import ReflexCaptureAgent
 from pacai.core.directions import Directions
-from pacai.core import distance
 from pacai.core.actions import Actions
-from pacai.core.search import search
-from pacai.student.searchAgents import AnyFoodSearchProblem
 
 
 class BetterDefensiveReflexAgent(ReflexCaptureAgent):
@@ -30,24 +26,29 @@ class BetterDefensiveReflexAgent(ReflexCaptureAgent):
             features['onDefense'] = 0
 
         # Computes distance to invaders we can see.
-        enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
-        invaders = [a for a in enemies if a.isPacman() and a.getPosition() is not None]
+        enemies = [successor.getAgentState(i)
+                   for i in self.getOpponents(successor)]
+        invaders = [a for a in enemies if a.isPacman(
+        ) and a.getPosition() is not None]
         features['numInvaders'] = len(invaders)
 
         if (len(invaders) > 0):
-            dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
+            dists = [self.getMazeDistance(
+                myPos, a.getPosition()) for a in invaders]
             features['invaderDistance'] = min(dists)
-    
-        enemyDists = [self.getMazeDistance(myPos, a.getPosition()) for a in enemies]
+
+        enemyDists = [self.getMazeDistance(
+            myPos, a.getPosition()) for a in enemies]
         features['enemyDistance'] = min(enemyDists)
 
         if (action == Directions.STOP):
             features['stop'] = 1
 
-        rev = Directions.REVERSE[gameState.getAgentState(self.index).getDirection()]
+        rev = Directions.REVERSE[gameState.getAgentState(
+            self.index).getDirection()]
         if (action == rev):
             features['reverse'] = 1
-        
+
         return features
 
     def getWeights(self, gameState, action):
@@ -59,7 +60,8 @@ class BetterDefensiveReflexAgent(ReflexCaptureAgent):
             'reverse': -2,
             'enemyDistance': -5
         }
-    
+
+
 class BetterOffensiveReflexAgent(ReflexCaptureAgent):
     """
     A reflex agent that seeks food.
@@ -92,13 +94,17 @@ class BetterOffensiveReflexAgent(ReflexCaptureAgent):
         # This should always be True, but better safe than sorry.
         if (len(foodList) > 0):
             # myPos = successor.getAgentState(self.index).getPosition()
-            minDistance = min([self.getMazeDistance(myNextPos, food) for food in foodList])
-            minDistanceNormalized = float(minDistance) / (walls.getWidth() * walls.getHeight())
+            minDistance = min([self.getMazeDistance(myNextPos, food)
+                              for food in foodList])
+            minDistanceNormalized = float(
+                minDistance) / (walls.getWidth() * walls.getHeight())
             features['distanceToFood'] = minDistanceNormalized
-        
+
         # Farther is better, squares each maze distance to pre
-        enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
-        enemyGhostsPos = [a.getNearestPosition() for a in enemies if not a.isPacman() and a.getPosition() is not None]
+        enemies = [successor.getAgentState(i)
+                   for i in self.getOpponents(successor)]
+        enemyGhostsPos = [a.getNearestPosition(
+        ) for a in enemies if not a.isPacman() and a.getPosition() is not None]
 
         # # Get distance to closest ghost
         # closestGhost = float('inf')
@@ -107,19 +113,19 @@ class BetterOffensiveReflexAgent(ReflexCaptureAgent):
         #         myPos, newGhostPos)
         #     if closestGhost > ghostDistanceToPac:
         #         closestGhost = ghostDistanceToPac
-        
+
         # closestGhostNormalized = float(closestGhost) / (walls.getWidth() * walls.getHeight())
         # features["closest-enemy-ghost"] = closestGhostNormalized
 
         # Count the number of ghosts 1-step away.
-        features["#-of-ghosts-1-step-away"] = sum((next_x, next_y) in
-                Actions.getLegalNeighbors(g, walls) for g in enemyGhostsPos)
+        numGOSA = sum((next_x, next_y) in Actions.getLegalNeighbors(
+            g, walls) for g in enemyGhostsPos)
+        features["#-of-ghosts-1-step-away"] = numGOSA
 
         # If there is no danger of ghosts then add the food feature.
         if features["#-of-ghosts-1-step-away"] == 0 and food[next_x][next_y]:
             features['eats-food'] = 1
 
-        
         # If there is no danger of ghosts then add the food feature.
         capsule = self.getCapsules(gameState)
         # print("capsule: ", capsule)
@@ -139,10 +145,11 @@ class BetterOffensiveReflexAgent(ReflexCaptureAgent):
 
         # dont revers on our side!
         if features['onOffense'] == -1:
-            rev = Directions.REVERSE[gameState.getAgentState(self.index).getDirection()]
+            rev = Directions.REVERSE[gameState.getAgentState(
+                self.index).getDirection()]
             if (action == rev):
                 features['reverse'] = 1
-        
+
         if (action == Directions.STOP):
             features['stop'] = 1
 
@@ -152,7 +159,8 @@ class BetterOffensiveReflexAgent(ReflexCaptureAgent):
     def getWeights(self, gameState, action):
         return {
             'bias': 100,
-            'successorScore': 20,  # may have to change this, 8 was based on losing a lot of score if you die
+            # may have to change this, 8 was based on losing a lot of score if you die
+            'successorScore': 20,
             'distanceToFood': -40,
             'closest-enemy-ghost': 2,
             # 'totalFoodCount': -10,
@@ -164,9 +172,10 @@ class BetterOffensiveReflexAgent(ReflexCaptureAgent):
             'stop': -30
         }
 
+
 def createTeam(firstIndex, secondIndex, isRed,
-        first = BetterOffensiveReflexAgent,
-        second = BetterDefensiveReflexAgent):
+               first=BetterOffensiveReflexAgent,
+               second=BetterDefensiveReflexAgent):
     """
     This function should return a list of two agents that will form the capture team,
     initialized using firstIndex and secondIndex as their agent indexed.
